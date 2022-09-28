5523a8084d4c65e15ef2ef771f10dcf4b5e44e0a
BowenHarkema <b.harkema@clappform.com>
2022-09-28 09:38:58 +0200

    Ready for push

---
+++ RealstatsModelRollout/src/vmachine.py
@@ -0,0 +1,22 @@
+import dev_enviroment
+
+class vmachine:
+    def init():
+        global baseURL
+        
+    def start_venv():
+        if dev_enviroment.DEV_PLATFORM == "Windows":
+            print("starting virtual machine for Windows")
+
+
+        elif dev_enviroment.DEV_PLATFORM == "Linux":
+            print("starting virtual machine for Linux")
+
+
+        elif dev_enviroment.DEV_PLATFORM == "MacOS":
+            print("starting virtual machine for MacOS")
+            
+
+
+
+vmachine.start_venv()
