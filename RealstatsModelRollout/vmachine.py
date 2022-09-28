3fd1514162958e4aebee313bd132bd55a64ad688
BowenHarkema <b.harkema@clappform.com>
2022-09-28 12:21:14 +0200

    trying to get the package to see dev_platform.py

--- RealstatsModelRollout/vmachine.py
+++ RealstatsModelRollout/vmachine.py
@@ -1,10 +1,13 @@
-import dev_enviroment
+from .dev_enviroment import dev_enviroment
 
 class vmachine:
     def init():
         global baseURL
         
     def start_venv():
+        dev_enviroment.system_scan()
+
+
         if dev_enviroment.DEV_PLATFORM == "Windows":
             print("starting virtual machine for Windows")
 
