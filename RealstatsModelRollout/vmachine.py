446795a6124b12defa4a4399de086d1860073ba2
BowenHarkema <b.harkema@clappform.com>
2022-09-28 12:54:45 +0200

    0.0.1.dev10

--- RealstatsModelRollout/vmachine.py
+++ RealstatsModelRollout/vmachine.py
@@ -2,24 +2,24 @@ from os import system
 import platform
 
 class vmachine:
-    def init():
-        global dev_platform 
-        global dev_platform_vers
-        global dev_platform_release
+    dev_platform = ""
+    dev_platform_vers = ""
+    dev_platform_release = ""
 
-        dev_platform = platform.system()
-        dev_platform_vers = platform.version()
-        dev_platform_release = platform.release()    
+    def init(self):
+        self.dev_platform = platform.system()
+        self.dev_platform_vers = platform.version()
+        self.dev_platform_release = platform.release()    
 
-    def start_venv():
-        if dev_platform == "Windows":
+    def start_venv(self):
+        if self.dev_platform == "Windows":
             print("starting virtual machine for Windows")
 
 
-        elif dev_platform == "Linux":
+        elif self.dev_platform == "Linux":
             print("starting virtual machine for Linux")
 
 
-        elif dev_platform == "MacOS":
+        elif self.dev_platform == "MacOS":
             print("starting virtual machine for MacOS")
             
