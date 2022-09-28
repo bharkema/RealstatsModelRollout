8d9975d6114e19e6d6df0611d0ebc5e9461ab49f
BowenHarkema <b.harkema@clappform.com>
2022-09-28 12:30:53 +0200

    now automate system scan

--- RealstatsModelRollout/dev_enviroment.py
+++ RealstatsModelRollout/dev_enviroment.py
@@ -7,6 +7,14 @@ class dev_enviroment:
         global DEV_PLATFORM_RELEASE
         global DEV_PLATFORM_VERSION
 
+        DEV_PLATFORM = platform.system()
+        DEV_PLATFORM_VERSION = platform.version()
+        DEV_PLATFORM_RELEASE = platform.release()
+
+        print("Platform: " + DEV_PLATFORM)
+        print("Platform version: " + DEV_PLATFORM_VERSION)
+        print("Platform release: " + DEV_PLATFORM_RELEASE)
+
     def system_scan():
         DEV_PLATFORM = platform.system()
         DEV_PLATFORM_VERSION = platform.version()
