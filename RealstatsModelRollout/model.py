aaed8ca27f2cf2db6b02c51031677ebb437ab5d4
bharkema <b.harkema@clappform.com>
2022-09-30 12:36:23 +0200

    rename of get info

--- RealstatsModelRollout/model.py
+++ RealstatsModelRollout/model.py
@@ -5,6 +5,6 @@ class Model:
     def __init__(self, name):
         self.id = name
         
-    def Get_Info(self, base_url, base_port):
+    def Get_info(self, base_url, base_port):
         respone = requests.get(url=base_url + ':' + base_port)
         return respone
