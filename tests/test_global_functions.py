0cddbdad2f820492fb36ca0e5d0f985383213d6e
BowenHarkema <b.harkema@clappform.com>
2022-10-25 16:42:55 +0200

    still wotking on testing automation

--- tests/test_global_functions.py
+++ tests/test_global_functions.py
@@ -1,5 +1,6 @@
 import pytest
 import RealstatsModelRollout as RMR
+from .settings import settings
 
 def test_Find_correct():
     assert RMR.globalFunctions.Find("test/string", "/") == [4]
@@ -8,4 +9,4 @@ def test_Find_incorrect():
     assert RMR.globalFunctions.Find("test/string", "/") != [2]
 
 def test_Path_correct():
-    assert RMR.globalFunctions.Path_is_dir("c:/") == "c:/"
+    assert RMR.globalFunctions.Path_is_dir(settings.Main_path) == settings.Main_path
