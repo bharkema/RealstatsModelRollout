7002e57dea721eefea15e528b96202d45f54b086
BowenHarkema <b.harkema@clappform.com>
2022-10-26 10:01:20 +0200

    another test

--- tests/test_global_functions.py
+++ tests/test_global_functions.py
@@ -10,3 +10,8 @@ def test_Find_incorrect():
 
 def test_Path_correct():
     assert RMR.globalFunctions.Path_is_dir(settings.Main_path) == settings.Main_path
+
+def test_Path_incorrect():
+    with pytest.raises(Exception) as exc:
+        RMR.globalFunctions.Path_is_dir("c:/")
+        assert "Given localpath: c:/ is not a directory" in str(exc.value)
