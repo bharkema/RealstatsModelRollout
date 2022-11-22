e1887792b4dd620bac63280c27213602e1605a4b
BowenHarkema <79573684+bharkema@users.noreply.github.com>
2022-11-22 13:50:09 +0100

    Pulled accesstoken and update to init

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,8 +7,5 @@ import os
 # VARS #
 # These are global vars and need to be filled in when you want to test the package. Then also need to be removed when pushing to git
 settings
-# settings.Gitaccesstoken = os.environ["API_KEY"]
-# settings.Main_path = os.environ["MAIN_PATH"]
-
-settings.Gitaccesstoken = "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"
-settings.Main_path = "c:/test/"
+settings.Gitaccesstoken = os.environ["API_KEY"]
+settings.Main_path = os.environ["MAIN_PATH"]
